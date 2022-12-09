8733abb08c585fd989e2c977e119f683a675f1cd
ntademaru <ntademaru@gmail.com>
2022-12-08 17:36:51 -0800

    Created full board with fixed tiles

--- src/components/board/board.jsx
+++ src/components/board/board.jsx
@@ -15,49 +15,75 @@ export default function Board(props) {
 
     return orientations[Math.floor(Math.random()* orientations.length)]
   }
+  const nonfixedTilesArray = [];
 
-  const nonfixedTiles = [
-    {
+  for (let i = 0; i < 34; i++) {
+    let tile = {
       spawn: 0,
-      treasure: "",
       orientation: getRandOrientation(),
-      shape: "A",
       extra: false,
-      used: false
-    },
-    {
-      spawn: 0,
-      treasure: "",
-      orientation: getRandOrientation(),
-      shape: "B",
-      extra: false,
-      used: false
-    },
-    {
-      spawn: 0,
-      treasure: "",
-      orientation: getRandOrientation(),
-      shape: "C",
-      extra: false,
-      used: false
-    },
-    {
-      spawn: 0,
-      treasure: "",
-      orientation: getRandOrientation(),
-      shape: "D",
-      extra: false,
-      used: false
-    },
-    {
-      spawn: 0,
-      treasure: "",
-      orientation: getRandOrientation(),
-      shape: "E",
-      extra: false,
-      used: false
-    },
-  ];
+      used: false,
+    }
+
+    if (i === 0) tile.extra = true; 
+    
+    switch (true) {
+      case (i < 15):
+        tile.shape = "L";
+        break;
+      case (i < 21):
+        tile.shape = "T";
+        break;
+      default:
+        tile.shape = "I"
+        break;
+    }
+
+    nonfixedTilesArray.push(tile)
+  }
+
+  // const nonfixedTiles = [
+  //   {
+  //     spawn: 0,
+  //     treasure: "",
+  //     orientation: getRandOrientation(),
+  //     shape: "A",
+  //     extra: false,
+  //     used: false
+  //   },
+  //   {
+  //     spawn: 0,
+  //     treasure: "",
+  //     orientation: getRandOrientation(),
+  //     shape: "B",
+  //     extra: false,
+  //     used: false
+  //   },
+  //   {
+  //     spawn: 0,
+  //     treasure: "",
+  //     orientation: getRandOrientation(),
+  //     shape: "C",
+  //     extra: false,
+  //     used: false
+  //   },
+  //   {
+  //     spawn: 0,
+  //     treasure: "",
+  //     orientation: getRandOrientation(),
+  //     shape: "D",
+  //     extra: false,
+  //     used: false
+  //   },
+  //   {
+  //     spawn: 0,
+  //     treasure: "",
+  //     orientation: getRandOrientation(),
+  //     shape: "E",
+  //     extra: false,
+  //     used: false
+  //   },
+  // ];
   const fixedBoard = [
     [{
       spawn: 1,
@@ -67,52 +93,138 @@ export default function Board(props) {
       extra: false,
       used: true
     }, null, {
+      spawn: 0,
+      treasure: "",
+      orientation: 270,
+      shape: "T",
+      extra: false,
+      used: true
+    }, null, {
+      spawn: 0,
+      treasure: "",
+      orientation: 270,
+      shape: "T",
+      extra: false,
+      used: true
+    }, null, {
       spawn: 2,
       treasure: "",
       orientation: 0,
       shape: "L",
       extra: false,
       used: true
-    }],
-    [null, null, null],
+    }
+  ],
+    [null, null, null, null, null, null, null],
     [{
-      spawn: 4,
+      spawn: 0,
+      treasure: "",
+      orientation: 0,
+      shape: "T",
+      extra: false,
+      used: true
+    }, null,{
+      spawn: 0,
+      treasure: "",
+      orientation: 270,
+      shape: "T",
+      extra: false,
+      used: true
+    }, null,{
+      spawn: 0,
+      treasure: "",
+      orientation: 180,
+      shape: "T",
+      extra: false,
+      used: true
+    }, null,{
+      spawn: 0,
+      treasure: "",
+      orientation: 180,
+      shape: "T",
+      extra: false,
+      used: true
+    }],
+    [null, null, null, null, null, null, null], 
+    [{
+      spawn: 0,
+      treasure: "",
+      orientation: 0,
+      shape: "T",
+      extra: false,
+      used: true
+    }, null,{
+      spawn: 0,
+      treasure: "",
+      orientation: 0,
+      shape: "T",
+      extra: false,
+      used: true
+    }, null,{
+      spawn: 0,
+      treasure: "",
+      orientation: 90,
+      shape: "T",
+      extra: false,
+      used: true
+    }, null,{
+      spawn: 0,
+      treasure: "",
+      orientation: 180,
+      shape: "T",
+      extra: false,
+      used: true
+    }],
+    [null, null, null, null, null, null, null],   
+    [{
+      spawn: 3,
       treasure: "",
       orientation: 180,
       shape: "L",
       extra: false,
       used: true
     }, null, {
-      spawn: 3,
+      spawn: 0,
+      treasure: "",
+      orientation: 90,
+      shape: "T",
+      extra: false,
+      used: true
+    }, null, {
+      spawn: 0,
+      treasure: "",
+      orientation: 90,
+      shape: "T",
+      extra: false,
+      used: true
+    }, null, {
+      spawn: 4,
       treasure: "",
       orientation: 270,
       shape: "L",
       extra: false,
       used: true
-    }]
-  ]; // matrix filled with every fixed tile
+    }
+  ]]; // matrix filled with every fixed tile
 
   // itr through fixed board
   // if we're on an empty spot pick random number in nonfixedTiles
 
-  for (let i = 0; i < fixedBoard.length; i++) {
-    for (let j = 0; j < fixedBoard.length;) {
-      const currTile = fixedBoard[i][j];
-      if (!currTile) {
-        let randNum = Math.floor(Math.random() * nonfixedTiles.length);
-        console.log(randNum)
-        console.log(nonfixedTiles[randNum].used)
-      
-        if (!nonfixedTiles[randNum].used) {
-          fixedBoard[i][j] = nonfixedTiles[randNum];
-          nonfixedTiles[randNum].used = true;
-          j++;
-        } 
-      } else {
-        j++;
-      }
-    }
-  }
+  // for (let i = 0; i < fixedBoard.length; i++) {
+  //   for (let j = 0; j < fixedBoard.length;) {
+  //     const currTile = fixedBoard[i][j];
+  //     if (!currTile) {
+  //       let randNum = Math.floor(Math.random() * nonfixedTilesArray.length);
+  //       if (!nonfixedTilesArray[randNum].used) {
+  //         fixedBoard[i][j] = nonfixedTilesArray[randNum];
+  //         nonfixedTilesArray[randNum].used = true;
+  //         j++;
+  //       } 
+  //     } else {
+  //       j++;
+  //     }
+  //   }
+  // }
 
   return (
     <div>
