3d71dd12865d2fc6f1fe1da7d3f4c33651dde144
ntademaru <ntademaru@gmail.com>
2022-12-13 00:12:17 -0800

    Fixed merge issue

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
-// import Board from './components/board/board';
-import Game from './components/game/game'
+import Board from './components/board/board';
+// import Game from './components/game/game'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <Board></Board>
+        {/* <Game></Game> */}
       </header>
     </div>
   );
--- src/components/tile/tile.js
+++ src/components/tile/tile.js
@@ -1,24 +1,76 @@
 import React, { useState, useEffect } from "react";
+import Canvas from "./canvas";
 import "./tile.css";
-
 export default function Tile(props) {
   const [spawn, setSpawn] = useState(null); // num
   const [treasure, setTreasure] = useState(null); // string
   const [orientation, setOrientation] = useState(null); // num
   const [shape, setShape] = useState(null); // string/char
   const [extra, setExtra] = useState(null); // bool
-  
+
+  const COLORS = [null, "red", "green", "yellow", "blue"];
+
+  const draw = ctx => {
+    switch (true) { // draw background
+      case (shape === "L"):
+        ctx.fillStyle = "#AB6B55";
+        ctx.fillRect(0, 0, 80, 80);
+        ctx.fillStyle = "#E3DFC0";
+        ctx.fillRect(20, 0, 40, 60);
+        ctx.fillStyle = "#E3DFC0";
+        ctx.fillRect(20, 20, 80, 40);
+        break;
+      case (shape === "I"):
+        ctx.fillStyle = "#AB6B55";
+        ctx.fillRect(0, 0, 80, 80);
+        ctx.fillStyle = "#E3DFC0";
+        ctx.fillRect(20, 0, 40, 80);
+        break;
+      default:
+        ctx.fillStyle = "#AB6B55";
+        ctx.fillRect(0, 0, 80, 80);
+        ctx.fillStyle = "#E3DFC0";
+        ctx.fillRect(0, 20, 80, 40);
+        ctx.fillStyle = "#E3DFC0";
+        ctx.fillRect(20, 20, 40, 80);
+        break;
+    }
+
+    if (spawn) { // draw spawn
+      ctx.beginPath();
+      ctx.arc(40, 40, 15, -4, Math.PI);
+      ctx.fillStyle = COLORS[spawn];
+      ctx.fill();
+      ctx.stroke();
+    } else if (treasure) { // draw treasure
+      ctx.beginPath();
+      ctx.moveTo(25, 25);
+      ctx.lineTo(55, 55);
+      ctx.moveTo(25, 55);
+      ctx.lineTo(55, 25);
+      ctx.stroke();
+    }
+  };
+
   useEffect(() => {
     if (props.tile) {
-      setSpawn(props.tile.spawn)
-      setTreasure(props.tile.treasure);
-      setOrientation(props.tile.orientation);
-      setShape(props.tile.shape);
-      setExtra(props.tile.extra);
+      const {
+        spawn,
+        treasure,
+        orientation,
+        shape,
+        extra
+      } = props.tile;
+      setSpawn(spawn)
+      setTreasure(treasure);
+      setOrientation(orientation);
+      setShape(shape);
+      setExtra(extra);
     }
   }, [props])
-
   return (
-    <p className="tile" style={{ transform: `rotate(${orientation}deg)` }}>{shape}</p>
+    <div>
+      <Canvas draw={draw} height={80} width={80} orientation={orientation}></Canvas>
+    </div>
   );
 }
